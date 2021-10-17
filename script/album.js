39bbca75997048c0d623d65558e7017f3cc49254
GaliaBonda <galina.rizhniak@gmail.com>
2021-10-17 14:34:25 +0300

    add active effect for album

--- script/album.js
+++ script/album.js
@@ -7,10 +7,21 @@ for (let i = 0; i < album.children.length; i++) {
 
 picArray.forEach((photo) => {
     photo.addEventListener('click', changeNotifications);
+    photo.addEventListener('mouseover', makePicActive);
+    photo.addEventListener('mouseout', makePicNonActive);
 })
 
 function changeNotifications(event) {
     const currentIndex = picArray.indexOf(event.target);
     const notifications = document.getElementById('notifications');
     notifications.setAttribute('data-after', currentIndex);
+}
+
+function makePicActive() {
+    this.style.cursor = 'pointer';
+    this.style.opacity = '0.5';
+}
+
+function makePicNonActive() {
+    this.style.opacity = '1';
 }
